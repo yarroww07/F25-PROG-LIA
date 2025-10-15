75577d614c0468658641f4801754f37f5abdb4f6
yarroww07 <raniabouladraf@gmail.com>
2025-10-15 02:53:52 -0400

    Testing out the first plot (male vs female sleep conditions!)

---
+++ Males_Females_Sleep_Conditions.py
@@ -0,0 +1,37 @@
+# Imports
+import pandas as pd
+import matplotlib.pyplot as plt
+
+Sleep_Data = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv")
+
+# Variables
+condition_count_male = 0
+condition_count_female = 0
+
+# Assigning variables to each row
+for index, row in Sleep_Data.iterrows():
+    gender = row['Gender']
+    condition = row['Sleep Disorder']
+    
+    # Checking if a condition is applied to male or female
+    if condition != 'None':
+        if gender == 'Male':
+            condition_count_male += 1
+        elif gender == 'Female':
+            condition_count_female += 1
+
+# TEST (keep as comment)
+# print(condition_count_female)
+# print(condition_count_male)
+
+# Creating a bar graph 
+# Variables
+categories = ['Male', 'Female']
+values = [condition_count_male, condition_count_female]
+
+plt.bar(categories, values)
+plt.title('Test')
+plt.xlabel('Gender')
+plt.ylabel('Number of People with Sleep Disorders')
+plt.grid()
+plt.show()
--- Pandas_Data_Import.py
+++ Pandas_Data_Import.py
@@ -1,5 +1,20 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 Sleep_Data = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv")
  
+
+print(Sleep_Data.columns)
+# Create the plot
+x = Sleep_Data['Sleep Duration']
+y = Sleep_Data['Quality of Sleep']
+# plt.scatter(x,y, color = 'blue', label = 'Data points')
+plt.plot(x, y, 'o', label='Data Points')
+plt.title('Sleep Quality vs Sleep Duration')
+plt.xlabel('Sleep Duration')
+plt.ylabel('Sleep Quality')
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.show()
 
