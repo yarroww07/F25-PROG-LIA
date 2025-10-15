76f34915552295915167ef5934f6001c61a31c5d
yarroww07 <raniabouladraf@gmail.com>
2025-10-15 02:55:09 -0400

    Deleting all test files

--- Data_Import.py
+++
@@ -1,6 +0,0 @@
-import numpy as np 
-import matplotlib.pyplot as plt
-
-gender, age, occupation, sleep_duration, sleep_quality, physical_activity, stress_level, bmi, blood_pressure, heart_rate, daily_steps, sleep_disorder = np.loadtxt(
-    'Sleep_health_and_lifestyle_dataset.csv', skiprows=1, usecols=(1,2,3,4,5,6,7,8,9,10,11,12), unpack=True)
-
--- Pandas_Data_Import.py
+++ Pandas_Data_Import.py
@@ -3,18 +3,3 @@ import matplotlib.pyplot as plt
 
 Sleep_Data = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv")
  
-
-print(Sleep_Data.columns)
-# Create the plot
-x = Sleep_Data['Sleep Duration']
-y = Sleep_Data['Quality of Sleep']
-# plt.scatter(x,y, color = 'blue', label = 'Data points')
-plt.plot(x, y, 'o', label='Data Points')
-plt.title('Sleep Quality vs Sleep Duration')
-plt.xlabel('Sleep Duration')
-plt.ylabel('Sleep Quality')
-plt.grid(True)
-plt.legend()
-plt.tight_layout()
-plt.show()
-
