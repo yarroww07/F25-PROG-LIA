b612eef7cd1f963c4860bc260519f0c615a9c4b1
sarahmeghdir <sarah.meghdir@gmail.com>
2025-10-17 13:57:41 -0400

    Update Correlation Between BMI Category and Sleep Duration.py

--- Graphs and filter/Correlation Between BMI Category and Sleep Duration.py
+++ Graphs and filter/Correlation Between BMI Category and Sleep Duration.py
@@ -1,7 +1,6 @@
 #CORRELATION BETWEEN BMI CATEGORIES AND SLEEP DURATION
-#BY SARAH MEGHDIR
+#AUTHOR: SARAH MEGHDIR
 #BAR GRAPH 
-
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,7 +8,6 @@ import matplotlib.pyplot as plt
 Sleep_Data = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv")
  
 #DEFINING MY VARIABLES
-
 Sleep_Data['BMI Category'] = Sleep_Data['BMI Category'].replace({'Normal Weight':'Normal'})
 x = Sleep_Data["BMI Category"]
 y = Sleep_Data["Sleep Duration"]
@@ -17,9 +15,10 @@ y = Sleep_Data["Sleep Duration"]
 
 #GRAPHING
 
-
+#BARS
 plt.bar(x, y, color='darkmagenta')
 
+#TITLES
 plt.title ('Correlation Between BMI Category and Sleep Duration')
 plt.xlabel('BMI Category')
 plt.ylabel('Sleep Duration')
