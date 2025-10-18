f0a997a39b5d94bfcdcf1c3e72b8f6cd875b0928
Meriemhamzi <meriem.hamzi@outlook.com>
2025-10-17 22:33:23 -0400

    updated version, refinig code and graph

--- (Histogram) Distribution of the daily steps.py
+++ (Histogram) Distribution of the daily steps.py
@@ -5,11 +5,15 @@ import matplotlib.pyplot as plt
 
 Sleep_Data = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv")
 
-
+#Variables
 x = Sleep_Data["Daily Steps"]
 
+#Plotting the histogram
+plt.figure(figsize = (12,6))
 plt.hist(x, color = "purple")
 plt.title('Daily Step distribution')
 plt.xlabel('Daily steps ')
+plt.ylabel('Count') #Number of individuals who did the daily steps 
+plt.tight_layout()
 plt.show()
  
