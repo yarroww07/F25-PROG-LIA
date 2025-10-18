65a47994477603af6251c415b22405d129a047a4
Meriemhamzi <meriem.hamzi@outlook.com>
2025-10-17 21:30:44 -0400

    blood pressure proportions pie chart

---
+++ Blood pressure proportions.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+Sleep_Data = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv")
+
+#Variables
+
+high_blood_pressure = 0
+normal_blood_pressure = 0
+low_blood_pressure = 0
+
+#Preparing the values for each variable
+for index, row in Sleep_Data.iterrows():
+    blood_pressure = row['Blood Pressure']
+    
+    systolic = int(blood_pressure.split('/')[0])
+    diastolic = int(blood_pressure.split('/')[1])
+    
+    if systolic == 120 and diastolic == 80:
+        normal_blood_pressure += 1
+    elif systolic > 120 or diastolic > 80:
+        high_blood_pressure += 1
+    else:
+        low_blood_pressure += 1
+        
+# Making of pir chart
+labels = ['Low Blood Pressure', 'Normal Blood Pressure', 'High Blood Pressure']
+variables = [low_blood_pressure, normal_blood_pressure, high_blood_pressure]
+colors = ['skyblue', 'lightgreen', 'salmon']
+
+plt.pie(variables, labels=labels, colors=colors)
+plt.title('Proportions of blood pressure levels in sample')
+plt.show()
--- Occupation and average sleep duration.py
+++ Occupation and average sleep duration.py
@@ -1,3 +1,5 @@
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -17,5 +19,7 @@ y = Sleep_Data["Sleep Duration"]
 plt.figure(figsize=(16,6))
 plt.title('Average sleep duration per occupation')
 sns.barplot(Sleep_Data, x = 'Occupation' , y = 'Sleep Duration' , palette= 'flare')
-plt.grid()
+plt.grid(axis='y')
+plt.tight_layout()
+plt.show()
 
